150076ed127bc414bb510e6bd1d70fc3fae6844c
Nikola Lichovski <nikola.licovski@gmail.com>
2021-05-31 20:52:17 +0200

    update

--- app/src/test/java/SILab2Test.java
+++ app/src/test/java/SILab2Test.java
@@ -13,23 +13,23 @@ class SILab2Test {
     void multipleCondition() {
         RuntimeException ex;
         ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(-1, 0, 0))));
-        assertEquals(ex.getMessage(), "The hours are smaller than the minimum");
+		assertEquals(ex.getMessage(), "The hours are smaller than the minimum");
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(25, 0, 0))));
-        assertEquals(ex.getMessage(), "The hours are grater than the maximum");
+		assertEquals(ex.getMessage(), "The hours are grater than the maximum");
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(0, -1, 0))));
-        assertEquals(ex.getMessage(), "The minutes are not valid!");
+		assertEquals(ex.getMessage(), "The minutes are not valid!");
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(0, 60, 0))));
-        assertEquals(ex.getMessage(), "The minutes are not valid!");
+		assertEquals(ex.getMessage(), "The minutes are not valid!");
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(0, 0, 60))));
-        assertEquals(ex.getMessage(), "The seconds are not valid");
+		assertEquals(ex.getMessage(), "The seconds are not valid");
 		assertEquals(Arrays.asList(0), newOb.function(Arrays.asList(new Time(0, 0, 0))));
-        assertEquals(Arrays.asList(24*3600), newOb.function(Arrays.asList(new Time(24, 0, 0))));
+		assertEquals(Arrays.asList(24*3600), newOb.function(Arrays.asList(new Time(24, 0, 0))));
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(24, 0, 1))));
-        assertEquals(ex.getMessage(), "The time is greater than the maximum");
+		assertEquals(ex.getMessage(), "The time is greater than the maximum");
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(24, 1, 0))));
-        assertEquals(ex.getMessage(), "The time is greater than the maximum");
+		assertEquals(ex.getMessage(), "The time is greater than the maximum");
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(24, 0, 1))));
-        assertEquals(ex.getMessage(), "The time is greater than the maximum");
+		assertEquals(ex.getMessage(), "The time is greater than the maximum");
     }
 
     @Test
@@ -37,16 +37,16 @@ class SILab2Test {
         RuntimeException ex;
 		assertEquals(new ArrayList<Integer>(), newOb.function(new ArrayList<Time>()));
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(-1, 0, 0))));
-        assertEquals(ex.getMessage(), "The hours are smaller than the minimum");
+		assertEquals(ex.getMessage(), "The hours are smaller than the minimum");
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(25, 0, 0))));
         assertEquals(ex.getMessage(), "The hours are grater than the maximum");
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(1, -1, 0))));
-        assertEquals(ex.getMessage(), "The minutes are not valid!");
+		assertEquals(ex.getMessage(), "The minutes are not valid!");
 		assertEquals(Arrays.asList(3600), newOb.function(Arrays.asList(new Time(1, 0, 0))));
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(1, 0, -1))));
         assertEquals(ex.getMessage(), "The seconds are not valid");
 		assertEquals(Arrays.asList(24*3600), newOb.function(Arrays.asList(new Time(24, 0, 0))));
 		ex = assertThrows(RuntimeException.class, () -> newOb.function(Arrays.asList(new Time(24, 1, 0))));
-        assertEquals(ex.getMessage(), "The time is greater than the maximum");
+		assertEquals(ex.getMessage(), "The time is greater than the maximum");
     }
 }
